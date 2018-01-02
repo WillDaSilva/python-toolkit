faaa980d5f8b3be560ddf1acbe18aaf9991749ea
Will Da Silva <will@willdasilva.xyz>
2018-01-02 02:06:59 -0500

    Add isFib() function definition & change fib()
    fib() start value is no longer alterable by a parameter. Should one wish to
    start at a different place in the sequence, they should advance the generator
    using consume()

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,14 @@
-def fib(start=0):
+from math import sqrt
+
+def fib():
     ''' Generates fibonacci number '''
-    a, b = ((0, 1), (1, 1))[bool(start)]
+    a, b = (0, 1)
     while True:
         yield a
         a, b = b, a + b
 
 def isFib(n):
     ''' Checks if n is a fibonacci number '''
-    return False
+    phi = 0.5 + 0.5 * sqrt(5.0)
+    a = phi * n
+    return n == 0 or abs(round(a) - a) < 1.0 / n
