63e23bd5c02a1fbbcdfaac417b686359b629f42d
Will Da Silva <will@willdasilva.xyz>
2018-01-02 02:11:42 -0500

    Remove reduce() from __init__.py & change order

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
-from functools import reduce
 from .fibonacci import fib, isFib
-from .pandigital import pan, isPan
-from .prime import prime, isPrime, primeFactor
+from .figurate import polygonal, isPolygonal
 from .misc import product, nPr, nCr, gcd, lcm, factor, numFactor, flatten, \
                   tail, consume, nth, pairwise, grouper, roundrobin, \
                   partition, powerset, unique_everseen, unique_justseen, \
                   first_true, digitize, isPalindrome, reverseInt
-from .figurate import polygonal, isPolygonal
+from .pandigital import pan, isPan
+from .prime import prime, isPrime, primeFactor
